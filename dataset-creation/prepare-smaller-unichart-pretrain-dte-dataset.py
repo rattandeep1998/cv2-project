9fc0cf2e7ab3b70e9b2dcf97cb231197ef62a221
rattandeep1998 <merattandeep@gmail.com>
2024-04-13 22:15:09 -0400

    refinements

--- dataset-creation/prepare-smaller-unichart-pretrain-dte-dataset.py
+++ dataset-creation/prepare-smaller-unichart-pretrain-dte-dataset.py
@@ -3,6 +3,19 @@ import random
 
 def split_dataset(data, train_percent, val_percent, output_path_prefix):
     total_entries = len(data)
+
+    # characteristic_count = sum(1 for entry in data if entry['table'].startswith('Years |'))
+    # print(f"Characteristic count: {characteristic_count}")
+    
+    filtered_data = [entry for entry in data if not entry['table'].startswith('Characteristic')]
+    filtered_entries = len(filtered_data)
+
+    print(f"Total entries: {total_entries}")
+    print(f"Entries after Filter: {filtered_entries}")
+
+    total_entries = filtered_entries
+    data = filtered_data
+
     train_size = int(total_entries * train_percent / 100)
     val_size = int(total_entries * val_percent / 100)
     
@@ -19,11 +32,14 @@ def split_dataset(data, train_percent, val_percent, output_path_prefix):
     with open(f'{output_path_prefix}_val.json', 'w') as f:
         json.dump(val_data, f, indent=4)
 
-file_path = '../data/unichart-pretrain/filtered_unichart_pretrain_datatable.json'
-output_path_prefix = '../data/unichart-pretrain/filtered_unichart_pretrain_datatable'
+# file_path = '../data/unichart-pretrain/filtered_unichart_pretrain_datatable.json'
+# output_path_prefix = '../data/unichart-pretrain/filtered_unichart_pretrain_datatable'
+
+file_path = '../../datasets/unichart-pretrain-data/filtered_unichart_pretrain_datatable.json'
+output_path_prefix = '../../datasets/unichart-pretrain-data/filtered_unichart_pretrain_datatable'
 
 # Load data from the JSON file
 with open(file_path, 'r') as file:
     formatted_data = json.load(file)
 
-split_dataset(formatted_data, train_percent=0.1, val_percent=0.005, output_path_prefix=output_path_prefix)
+split_dataset(formatted_data, train_percent=5, val_percent=0.5, output_path_prefix=output_path_prefix)
